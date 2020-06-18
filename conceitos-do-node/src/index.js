bea4a86bb35c2b174ca6a29967381ee41cf94d77
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-06-18 20:51:58 -0300

    metodos http

--- conceitos-do-node/src/index.js
+++ conceitos-do-node/src/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+app.get('/projects', (request, response) => {
+  return response.json([
+    'project 1',
+    'project 2',
+  ])
+})
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'project 1',
+    'project 2',
+    'project 3',
+  ])
+})
+
+app.put('/projects:id', (request, response) => {
+  return response.json([
+    'project 4',
+    'project 2',
+    'project 3',
+  ])
+})
+
+app.delete('/projects:id', (request, response) => {
+  return response.json([
+    'project 2',
+    'project 3',
+  ])
 })
 
 app.listen(3333, () => {
