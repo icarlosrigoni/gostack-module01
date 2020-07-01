ba082395875bb0f66a4f6f708476f20cdaccd611
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2020-07-01 20:36:00 -0300

    Diferenças do ReactJS

--- 04-conceitos-do-react-native/src/index.js
+++ 04-conceitos-do-react-native/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
-import { View } from 'react-native' 
+import { View, Text, StyleSheet, StatusBar } from 'react-native' 
 
 export default function App() {
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor='#7159c1'/>
+      <View style={styles.container}>
+        <Text style={styles.title} >Hello Gostack</Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7159c1',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: "center"
+  },
+
+  title: {
+    fontSize: 32,
+    color: '#FFF',
+    fontWeight: 'bold',
+  }
+})
